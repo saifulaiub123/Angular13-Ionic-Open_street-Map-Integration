9b766d38c0005feb7e208a97fa21a1fdc712e95b
Saiful Islam <saifulprogrammer@gmail.com>
2022-04-12 09:04:00 +0600

    ngx-leaflet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { MapComponent } from './component/map/map.component';
 
 @NgModule({
   declarations: [AppComponent],
   exports: [],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, LeafletModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/member-ordering */
+/* eslint-disable max-len */
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, SimpleChanges, OnChanges  } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
-import * as L from 'leaflet';
+import {Browser, icon, latLng, LeafletMouseEvent, map, Map, MapOptions, marker, Routing, tileLayer} from 'leaflet';
 import 'leaflet-routing-machine';
 import { ILocationModel } from 'src/app/@core/model/IlocationModel';
+import { MapPoint } from 'src/app/@core/model/map-point-model';
 
 @Component({
   selector: 'app-map',
@@ -14,10 +17,13 @@ export class MapComponent implements OnInit, AfterViewInit, OnChanges  {
   @Input() routeStartingPoint: ILocationModel = null;
   @Input() routeEndPoint: ILocationModel = null;
 
-  map: L.Map;
+  map: Map;
+  mapPoint: MapPoint;
+  mapOptions: MapOptions;
   // eslint-disable-next-line @typescript-eslint/member-ordering
   @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
+  lastLayer: any;
 
   constructor() { }
 
@@ -26,7 +32,10 @@ export class MapComponent implements OnInit, AfterViewInit, OnChanges  {
   }
 
   ngOnInit() {
+    this.initializeDefaultMapPoint();
+    this.initializeDefaultOptions();
   }
+
 
   async ngAfterViewInit() {
 
@@ -37,25 +46,19 @@ export class MapComponent implements OnInit, AfterViewInit, OnChanges  {
       zoom: 15
     };
 
-    const map = L.map(this.mapContainer.nativeElement).setView([initialState.lat, initialState.lng], initialState.zoom);
+    // map(this.mapContainer.nativeElement).setView([initialState.lat, initialState.lng], initialState.zoom);
 
-    const myAPIKey = 'ba63517931d94498b2961f2aa4ba7eb8';
-    const isRetina = L.Browser.retina;
-
-    //const baseUrl = 'https://maps.geoapify.com/v1/tile/osm-bright-grey/{z}/{x}/{y}.png?apiKey=ba63517931d94498b2961f2aa4ba7eb8';
-    // const baseUrl = 'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=ba63517931d94498b2961f2aa4ba7eb8';
-    //const retinaUrl = 'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey=ba63517931d94498b2961f2aa4ba7eb8';
     // const baseUrl = 'https://tile.openstreetmap.org/${z}/${x}/${y}.png';
     const baseUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 
-    L.tileLayer(baseUrl, {
-      // eslint-disable-next-line max-len
-      attribution: '© OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
-      // apiKey: myAPIKey,
-      maxZoom: 20,
-      id: 'osm-bright',
-    } as any)
-    .addTo(map);
+    // tileLayer(baseUrl, {
+    //   // eslint-disable-next-line max-len
+    //   attribution: '© OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>',
+    //   // apiKey: myAPIKey,
+    //   maxZoom: 20,
+    //   id: 'osm-bright',
+    // } as any)
+    // .addTo(map);
 
 
 
@@ -67,63 +70,122 @@ export class MapComponent implements OnInit, AfterViewInit, OnChanges  {
 
   if(!this.routeStartingPoint == null && !this.routeEndPoint == null)
   {
-      const routing= L.Routing.control({
-        // router: L.Routing.osrmv1({
-        //     serviceUrl: 'http://router.project-osrm.org/route/v1/'
-        // }),
-        showAlternatives: true,
-            lineOptions : {
-            styles: [
-              {color: 'blue', opacity: 0.15, weight: 9},
-              {color: 'blue', opacity: 0.8, weight: 6},
-              {color: 'blue', opacity: 1, weight: 2}
-          ],
-          missingRouteStyles: [
-              {color: 'black', opacity: 0.5, weight: 7},
-              {color: 'white', opacity: 0.6, weight: 4},
-              {color: 'gray', opacity: 0.8, weight: 2, dashArray: '7,12'}
-          ],
-            extendToWaypoints : false,
-            missingRouteTolerance : 1
-        },
-        fitSelectedRoutes: true,
-        altLineOptions: {
-          styles: [{color: '#4c87e1', weight: 10}],
-          extendToWaypoints : true,
-          missingRouteTolerance : 1
-        },
-        show: false,
-        routeWhileDragging: true,
-        waypoints: [
-            L.latLng(23.691879, 90.457882),
-            L.latLng(23.7093978, 90.433806)
-        ],
-        useZoomParameter: true,
-        autoRoute: true,
-      }).addTo(map);
+    //   const routing= Routing.control({
+    //     // router: L.Routing.osrmv1({
+    //     //     serviceUrl: 'http://router.project-osrm.org/route/v1/'
+    //     // }),
+    //     showAlternatives: true,
+    //         lineOptions : {
+    //         styles: [
+    //           {color: 'blue', opacity: 0.15, weight: 9},
+    //           {color: 'blue', opacity: 0.8, weight: 6},
+    //           {color: 'blue', opacity: 1, weight: 2}
+    //       ],
+    //       missingRouteStyles: [
+    //           {color: 'black', opacity: 0.5, weight: 7},
+    //           {color: 'white', opacity: 0.6, weight: 4},
+    //           {color: 'gray', opacity: 0.8, weight: 2, dashArray: '7,12'}
+    //       ],
+    //         extendToWaypoints : false,
+    //         missingRouteTolerance : 1
+    //     },
+    //     fitSelectedRoutes: true,
+    //     altLineOptions: {
+    //       styles: [{color: '#4c87e1', weight: 10}],
+    //       extendToWaypoints : true,
+    //       missingRouteTolerance : 1
+    //     },
+    //     show: false,
+    //     routeWhileDragging: true,
+    //     waypoints: [
+    //         latLng(23.691879, 90.457882),
+    //         latLng(23.7093978, 90.433806)
+    //     ],
+    //     useZoomParameter: true,
+    //     autoRoute: true,
+    //   }).addTo(map);
 
-      routing.on('routesfound', (e) => {
-        const distance = e.routes[0].summary.totalDistance;
-        console.log('Distance ' + distance);
-    });
+    //   routing.on('routesfound', (e) => {
+    //     const distance = e.routes[0].summary.totalDistance;
+    //     console.log('Distance ' + distance);
+    // });
   }
   else{
-    const century21icon = L.icon({
+    const century21icon = icon({
       iconUrl: 'https://nominatim.openstreetmap.org/ui/build/images/marker-icon.png',
       iconSize: [30, 50]
       });
     // eslint-disable-next-line max-len
-    const marker = L.marker([initialState.lat, initialState.lng],{title: 'Pickup Point',icon: century21icon, draggable : true}).addTo(map);
+    //const marker = marker([initialState.lat, initialState.lng],{title: 'Pickup Point',icon: century21icon, draggable : true}).addTo(map);
   }
-
-
-    setTimeout(() => {
-      map.invalidateSize();
-    }, 800);
+  // setTimeout(() => {
+  //   this.map.invalidateSize();
+  // }, 800);
   }
 
 
   getCurrentPosition = async () => await Geolocation.getCurrentPosition();
+
+
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    public initializeMap(map: Map) {
+      this.map = map;
+      this.createMarker();
+    }
+
+  private async initializeDefaultMapPoint(){
+    const currPosition = await this.getCurrentPosition();
+    this.mapPoint ={
+      name : 'Map',
+      latitude: currPosition.coords.latitude,
+      longitude: currPosition.coords.longitude
+    };
+  }
+
+  private initializeDefaultOptions()
+  {
+    this.mapOptions = {
+      zoom: 15,
+      layers: [
+        tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '© OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'})
+      ]
+    };
+  }
+
+  private onMapClick(e: LeafletMouseEvent) {
+    this.clearMap();
+    this.updateMapPoint(e.latlng.lat, e.latlng.lng);
+    this.createMarker();
+  }
+
+  private updateMapPoint(latitude: number, longitude: number, name?: string) {
+    this.mapPoint = {
+      latitude,
+      longitude,
+      name: name ? name : this.mapPoint.name
+    };
+  }
+
+  public createMarker(){
+    this.clearMap();
+    const mapIcon = this.getDefaultIcon();
+    const coordinates = latLng([this.mapPoint.latitude, this.mapPoint.longitude]);
+    this.lastLayer = marker(coordinates).setIcon(mapIcon).addTo(this.map);
+    this.map.setView(coordinates, this.map.getZoom());
+  }
+
+  private getDefaultIcon() {
+    return icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png'
+    });
+  }
+  private clearMap() {
+    if (this.map.hasLayer(this.lastLayer)) {
+      this.map.removeLayer(this.lastLayer);
+    }
+  }
 
 }
 
